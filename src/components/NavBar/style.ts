9e5d5771de0a26a006e95eeba8cb1e6b435bfb7f
Giliard Silva <ghssilva248@gmail.com>
2024-12-04 22:15:16 -0300

    color navbar

--- src/components/NavBar/style.ts
+++ src/components/NavBar/style.ts
@@ -6,11 +6,14 @@ export const NavBar = styled.nav`
     justify-content: space-between;
     align-items: center;
 
-    padding: 1rem 1.5rem;
 
+    padding: 1rem 1.5rem;
+  
     border-bottom:1px solid #5E6472;
+    background: #000;
 
     font-family: "Bebas Neue", sans-serif;
     font-weight: 400;
     font-style: normal;
+
 `
