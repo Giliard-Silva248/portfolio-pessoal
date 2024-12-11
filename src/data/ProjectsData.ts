46606dd08e6e7fdd29057a7107b6faf10201ddf3
Giliard Silva <ghssilva248@gmail.com>
2024-12-11 18:50:09 -0300

    new project

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -5,6 +5,7 @@ import linksSociais from '../assets/projectsImg/linksSociais.png'
 import myShop from '../assets/projectsImg/myShop.png'
 import pagePsi from '../assets/projectsImg/pagePsi.png'
 import myCoin from '../assets/projectsImg/myCoin.png'
+import photographer from '../assets/projectsImg/photographer.png'
 
 export interface ProjectData{
     id: number,
@@ -29,30 +30,36 @@ export const ProjectsData:ProjectData[] = [
     },
     {
         "id": 3,
+        "img": photographer,
+        "title": "Arte em Cada Clique",
+        "url": "https://giliard-silva248.github.io/photographer/"
+    },
+    {
+        "id": 4,
         "img": consultaCepImg,
         "title": "Consulta Cep",
         "url": "https://giliard-silva248.github.io/consulta_cep/"
     },
     {
-        "id": 4,
+        "id": 5,
         "img": costureirasDoBairroImg,
         "title": "Costureiras do Bairro",
         "url": "https://giliard-silva248.github.io/costureiras-do-bairro/"
     },
     {
-        "id": 5,
+        "id": 6,
         "img": huddleImg,
         "title": "Huddle | Front-end Mentor",
         "url": "https://giliard-silva248.github.io/huddle/"
     },
     {
-        "id": 6,
+        "id": 7,
         "img": linksSociais,
         "title": "Links Sociais | Front-end Mentor",
         "url": "https://giliard-silva248.github.io/links-sociais/"
     },
     {
-        "id": 7,
+        "id": 8,
         "img": myShop,
         "title": "MyShop.com",
         "url": "https://giliard-silva248.github.io/ecommerce/"
